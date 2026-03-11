674681d4305448f780912f32d864ce7b46c53c13
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-11 16:02:54 +0100

    weather data fetched and displayed

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ fetch(
 		document.getElementById("author").textContent = `By: ${data.user.name}`;
 	})
 	.catch((err) => {
-		// Use a default background image/author
 		document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`;
 		document.getElementById("author").textContent = `By: Dodi Achmad`;
@@ -35,7 +34,7 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
 
 function getCurrentTime() {
 	const date = new Date();
-	document.querySelector(".time").textContent = date.toLocaleTimeString(
+	document.getElementById("time").textContent = date.toLocaleTimeString(
 		"en-us",
 		{ timeStyle: "short" },
 	);
@@ -43,17 +42,23 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000);
 
-/* if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(
-		(position) => {
-			console.log("Latitude:", position.coords.latitude);
-			console.log("Longitude:", position.coords.longitude);
-		},
-		(error) => {
-			console.error("Error getting location:", error.message);
-		},
-	);
-} else {
-	console.log("Geolocation is not supported by this browser.");
-}
- */
+navigator.geolocation.getCurrentPosition((position) => {
+	fetch(
+		`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`,
+	)
+		.then((res) => {
+			if (!res.ok) {
+				throw Error("Weather data not available");
+			}
+			return res.json();
+		})
+		.then((data) => {
+			const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+			document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `;
+		})
+		.catch((err) => console.error(err));
+});
