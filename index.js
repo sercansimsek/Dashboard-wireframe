ddd009fbeea75d3197c3f9cfce531196f9d19302
Sercan Şimşek <sercansimsek44@gmail.com>
2026-03-11 14:28:46 +0100

    crypto corner implemented

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",
-)
-  .then((response) => response.json())
-  .then((data) => {
-    document.querySelector(".body").style.backgroundImage =
-      `url(${data.urls.regular})`;
-    document.querySelector(".author").textContent = `By: ${data.user.name}`;
-  })
-  .catch((err) => {
-    // Use a default background image/author
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`;
-    document.querySelector(".author").textContent = `By: Dodi Achmad`;
-  });
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        // Use a default background image/author
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By: Dodi Achmad`
+    })
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Something went wrong!");
-    }
-    return response.json();
-  })
-  .then((data) => {
-    document.querySelector('.crypto').innerHTML = `
-      <img src=${data.image.thumb} alt='${data.name} icon'>
-      <p>${data.name}</p>
-    `
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
